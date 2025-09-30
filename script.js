778e96a8af51b99167b3ca8416bbcbb384080fc3
William7-16 <castillowilliann@gmail.com>
2025-09-30 10:00:10 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+        function handleSubmit(event) {
+            event.preventDefault(); // Evita el envío del formulario
+            // Redirige a la página de confirmación
+            window.location.href = 'Gracias html.html'; // Cambia esto al nombre de tu archivo de confirmación
+        }
